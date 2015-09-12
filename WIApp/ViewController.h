5ae3f444b1a1a462da78f3eb9a87a24b8c5605ab
Avinash <avinashb123@gmail.com>
2015-09-12 13:36:03 -0400

    Initial Commit

---
+++ WIApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WIApp
+//
+//  Created by admin on 9/12/15.
+//  Copyright © 2015 infy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WIApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WIApp
+//
+//  Created by admin on 9/12/15.
+//  Copyright © 2015 infy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
